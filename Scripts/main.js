c905add13617e9920ec6425cb6f6b19cb57af989
Tobiascodecraft <155187808+Tobiascodecraft@users.noreply.github.com>
2024-03-06 17:46:35 +0100

    Add files via upload

---
+++ Scripts/main.js
@@ -0,0 +1,41 @@
+let MainImage = document.querySelector(".click-img");
+let paragraph_1 = document.querySelector(".p-1-js");
+let paragraph_2 = document.querySelector(".p-2-js");
+let paragraph_3 = document.querySelector(".p-3-js");
+
+
+function Change1 () {
+    MainImage.src = "./Images/Click Images/success-story-3.png";
+    paragraph_1.innerHTML = 
+    "How They Did It: Seamless migration of thousands of legacy websites in just a few months, using APIs to add metadata and create URL redirects.";
+
+    paragraph_2.innerHTML = 
+    "The Impact: Achieved their goals of offering a quality, industry-leading product and faster site creation & management at scale.";
+
+    paragraph_3.innerHTML = 
+    "Favorite Flex Frame Tool: Connected Data to prepopulate site templates with structured customer data.";
+}
+
+function Change2 () {
+    MainImage.src = "./Images/Click Images/winning-img.jpg";
+    paragraph_1.innerHTML = 
+    "How They Did It: Reduced average site build times by 75%; increased revenue with expanded services; improved lifetime customer value.";
+
+    paragraph_2.innerHTML = 
+    "The Impact: Successful team onboarding in 2-3 days, compared to 2-3 weeks on WordPress; design tools that reduced development work from 15 days to 5 hours!";
+
+    paragraph_3.innerHTML = 
+    "Favorite Flex Frame Tool: Personalization engine for serving targeted content by audience.";
+}
+
+function Change3 () {
+    MainImage.src = "./Images/Click Images/solution-img.jpg";
+    paragraph_1.innerHTML = 
+    "How They Did It: Achieved thier goals of offering a quality, industry-leading product and faster site cration & management at scale.";
+
+    paragraph_2.innerHTML = 
+    "The Impact: Seamless migration of thousandes of legacy websites in just a few months, using APIs to add metadata and create URL redirects.";
+
+    paragraph_3.innerHTML = 
+    "Favorite Flex Frame Tool: Connected Flex Frame to prepopulate site templates with constructed customer data.";
+}
